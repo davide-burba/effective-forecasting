2fad80b9b5bd84a347592f9e22f3175c10086e86
Davide <davide.burba@datapred.com>
2023-06-15 09:57:26 +0200

    Add installed apps to settings

--- tutorials/api-example-django/project/project/settings.py
+++ tutorials/api-example-django/project/project/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "gtrends.apps.GtrendsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
