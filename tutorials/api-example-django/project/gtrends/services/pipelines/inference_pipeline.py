1bf7667d9845e85f3f13cdac199a87289d5f6eba
Davide <davide.burba@datapred.com>
2023-06-15 09:00:06 +0200

    Split pipeline_ml module in two

--- tutorials/api-example-django/project/gtrends/services/pipelines/inference_pipeline.py
+++ tutorials/api-example-django/project/gtrends/services/pipelines/inference_pipeline.py
@@ -1,28 +1,8 @@
 from typing import Dict
 
-from gtrends.ml import load_engine
-from gtrends.models import MLModel, MLModelVersion
-from gtrends.tasks import (
-    build_x_latest,
-    load_data,
-    preprocess,
-    save_mlmodelversion,
-    train,
-)
-
-
-def train_pipeline(ml_model: MLModel) -> MLModelVersion:
-    target_ts = ml_model.data_config.targets.all()
-    feature_ts = ml_model.data_config.features.all()
-    prep_params = ml_model.preprocess_config.params
-    model_params = ml_model.ml_config.params
-
-    data, metadata = load_data(target_ts, feature_ts)
-    x, y = preprocess(data, prep_params)
-    engine = train(x, y, model_params)
-    ml_model_version = save_mlmodelversion(engine, ml_model, metadata)
-
-    return ml_model_version
+from gtrends.models import MLModel
+from gtrends.services.ml import load_engine
+from gtrends.services.tasks import build_x_latest, load_data
 
 
 def inference_pipeline(ml_model: MLModel) -> Dict:
